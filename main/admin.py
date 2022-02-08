21a744ca13592efe8b1de03164c3348e117f39b0
gulmira <g.djumadylova@gmail.com>
2022-02-08 17:00:54 +0600

    ViewSet, Router

---
+++ main/admin.py
@@ -0,0 +1,18 @@
+from django.contrib import admin
+
+from main.models import CodeImage, Problema, Reply, Comment
+
+
+class ImageInline(admin.TabularInline):
+    model = CodeImage
+    min_num = 1
+    max_num = 5
+
+
+@admin.register(Problema)
+class ProblemAdmin(admin.ModelAdmin):
+    inlines = [ImageInline]
+
+
+admin.site.register(Reply)
+admin.site.register(Comment)
